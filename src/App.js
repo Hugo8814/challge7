886e6289cf7d4ccfa1f57329a4c1775e1e0b3f92
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-03-15 18:40:33 +0000

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TipCalculator />
     </div>
   );
 }
 
 export default App;
+
+function TipCalculator() {
+  const [tip, setTip] = useState(null);
+  const [bill, setbill] = useState("");
+
+  return (
+    <div>
+      <Bill bill={bill} setbill={setbill} />
+      <Tip>how did you like your service?</Tip>
+      <Tip> how did your friend like your service?</Tip>
+      <Total />
+      <Reset />
+    </div>
+  );
+}
+function Bill({ bill, setbill }) {
+  return (
+    <div>
+      <label>How much was your bill?</label>
+      <input
+        type="number"
+        placeholder="bill value"
+        value={bill}
+        onChange={(e) => setbill(e.target.value)}
+      />
+    </div>
+  );
+}
+
+function Tip({ children, setTip }) {
+  return (
+    <div>
+      <label>{children}</label>
+
+      <select onChange={(e) => setTip(e.target.value)}>
+        <option value="0">not good (0%)</option>
+        <option value="10">good (10%)</option>
+        <option value="15">geat (15%)</option>
+        <option value="20">excellent (20%)</option>
+        <option value="25">amazing (25%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Total() {
+  return <h3>{`Your pay  x ( $a + $b Tip)`} </h3>;
+}
+
+function Reset() {
+  return <button>Reset</button>;
+}
